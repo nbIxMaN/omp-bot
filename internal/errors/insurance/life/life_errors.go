f9e34ba31d15863af6aa0dff1142a6c36ddb81be
nbIxMaN <Shulllik@gmail.com>
2021-10-25 14:36:52 +0300

    [task-1] Create Help, Get, List, Delete, New, Edit command for insurance__life

--- internal/errors/insurance/life/life_errors.go
+++ internal/errors/insurance/life/life_errors.go
@@ -2,20 +2,8 @@ package life
 
 import "errors"
 
-const (
-	notFoundError          = "not found"
-	lastPageError          = "last page"
-	incorrectPositionError = "incorrect position"
+var (
+	NotFoundError          = errors.New("not found")
+	LastPageError          = errors.New("last page")
+	IncorrectPositionError = errors.New("incorrect position")
 )
-
-func NotFoundError() error {
-	return errors.New(notFoundError)
-}
-
-func LastPageError() error {
-	return errors.New(lastPageError)
-}
-
-func IncorrectPositionError() error {
-	return errors.New(incorrectPositionError)
-}
